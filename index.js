ddcbaa7ebec3bf400e53becd888c3cbdc7b77d58
Shiv Kumar Mahto <shivmahto37@gmail.com>
2023-10-23 15:42:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.post("/webhook",(req,res)=>{ //i want some
     let body_param=req.body;
 
     console.log(JSON.stringify(body_param,null,2));
-
+    console.log(body_param.entry[0].changes[0].value.messages[0]);
     if(body_param.object){
         console.log("inside body param");
         if(body_param.entry && 
@@ -48,7 +48,7 @@ app.post("/webhook",(req,res)=>{ //i want some
                let from = body_param.entry[0].changes[0].value.messages[0].from; 
                let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
-               console.log(body_param.entry[0].changes[0].value.messages[0].type);
+               // console.log(body_param.entry[0].changes[0].value.messages[0].type);
                
             
                axios({
